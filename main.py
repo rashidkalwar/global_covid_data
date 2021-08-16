8d351bc3cb73a8c9e5dd7fac495d3a8d13009271
Rashid Ali <80094039+rashidkalwar@users.noreply.github.com>
2021-08-16 07:55:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import pandas as pd
 import requests
 
 API_URL = "https://corona.lmao.ninja/v2/countries?yesterday&sort"
-response = requests.get(API_URL)
-data = response.json()
-df = pd.DataFrame(data)
-df.to_csv("Data.csv", index=False)
+
+
+def get_covid_csv(URL: str) -> None:
+    response = requests.get(API_URL)
+    data = response.json()
+    df = pd.DataFrame(data)
+    df.to_csv("Data.csv", index=False)
+
+
+if __name__ == "__main__":
+    get_covid_csv(API_URL)
